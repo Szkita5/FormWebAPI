cb191a0ae7dcf53b6efa33ff666c61f4e13173a5
Szkita5 <szergej3@gmail.com>
2020-11-10 14:23:17 +0000

    .

---
+++ Backend/WebAPI1/Data/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI1.Models;
+
+namespace WebAPI1.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext (DbContextOptions<EmployeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPI1.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Backend/WebAPI1/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset Birthday { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
